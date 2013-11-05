d3a9ca56c45754ed65793d0d319a5c90981083ed
Angel Lin <angel.tian.lin@gmail.com>
2013-11-05 04:10:51 -0500

    Platformer with water. My own modification to the modding week 9 post

---
+++ Assets/scripts/PlatformerAnimation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformerAnimation : MonoBehaviour {
+
+    public Rigidbody myRigidbody;
+	// assign in inspector
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        // if the player is moving
+        if ( Input.GetAxis( "Vertical" ) != 0f ) {
+            animation["Walk"].speed = myRigidbody.velocity.magnitude * 0.1f;
+            animation.CrossFade( "Walk" );
+		}
+			
+		//if ( Platformer.grounded == true) {            
+				
+            else if ( Input.GetKeyDown( KeyCode.Space ) ) {
+            animation.Play ("Jump");    
+            }
+        //}
+		
+        else {
+            animation.CrossFade( "Idle" );
+        }
+
+
+            // is the player holding down any buttons?
+            // remember our last transform.position in a variable, then take our current one, compare distance
+            // grab the rigidbody.velocity and look at magnitude
+
+        // then use animation.CrossFade() to play the walk animation
+
+        // ... ELSE, play idle animation
+
+	}
+}
---
+++ Assets/scripts/WaterScroll.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterScroll : MonoBehaviour {
+
+    // this code is ripped almost entirely from the Unity documentation for Material.mainTextureOffset
+    public float scrollSpeed = 0.5f;
+
+    void Update() {
+        float offset = Time.time * scrollSpeed;
+        renderer.material.mainTextureOffset = new Vector2(-offset, Mathf.Sin(offset * 2f) ); 
+    }
+}
